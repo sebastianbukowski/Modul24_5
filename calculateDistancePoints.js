fb5725f2fc08d814c0be8e7656b3adc51f82fb33
SebastianBukowski <seb-27@o2.pl>
2019-05-12 20:11:52 +0200

    Add the code together with the tests

--- calculateDistancePoints.js
+++ calculateDistancePoints.js
@@ -1,5 +1,53 @@
 const calculateDistancePoints = (distance, hillSize, kPoint) => {
-    ...
-  };
+  
+  // Wielkość skoczni (normalna 60 pnk., duża 60 pnk., mamucia 120 pnk.)
+  // Długość skoku (zaokrąglana do pół metra)
+  // Punkt "K" (inny dla każdej skoczni)
+  // Jest pięciu rzuri
+  // Dla skoczni normalnej są to 2 punkty za każdy metr różnicy między odległością a punktem "K"
+  // dla skoczni dużej jest to 1.8 punktu
+  // dla skoczni mamuciej jest to 1.2 punktu
+  // ważne jest to, że nota każdego sędziego jest liczbą między 0 a 20 (włącznie) i może być "połówką" (czyli np. 15.5) 
+  // dwie skrajne noty (minimalną oraz maksymalną) się odrzuca, a następnie sumuje pozostałe trzy noty
+  // punkty dodatkowe (2 punkty za każdy metr)
+  // const hillsData = [
+  //   {
+  //     hillSize: "normal",
+  //     defaultPoints: 60,
+  //     pointsPerMeter: 2,
+  //     kPoint: 98
+  //   },
+  //   {
+  //     hillsize: "large",
+  //     defaultPoints: 120,
+  //     pointsPerMeter: 1.8,
+  //     kPoint: 120
+  //   },
+  //   {
+  //     hillsize: "mamucia",
+  //     defaultPoints: 120,
+  //     pointsPerMeter: 1.2,
+  //     kPoint: 200
+  //   }
+  // ]
+  // const large = 120;
+  // const mamucia = 200;
+  // const dots;
+  // };
+  if (hillSize === 'normal') {
+    return (distance - kPoint) * 2 + 60;
+	}
+	else if (hillSize === 'large') {
+		return (distance - kPoint) * 1.8 + 60;
+	}
+	else if (hillSize === 'flying') {
+		return (distance - kPoint) * 1.2 + 120;
+	}
+	else {
+		return 'Wrong hill size';
+	}
+};
 
   module.exports = calculateDistancePoints;
+
+
--- calculateStylePoints.js
+++ calculateStylePoints.js
@@ -1,5 +1,14 @@
 const calculateStylePoints = (styleNotes) => {
-    ...
-  };
+//  Punkty za wiatr oraz belkę to dodatkowe punkty, której mają rekompensować warunki na skoczni i przez to     sprawiać, że wyniki są bardziej sprawiedliwe. Jeśli skoczkowi wiatr wieje pod narty, to wtedy dostanie        punkty ujemne (bo łatwiej  mu dalej dolecieć, więc jego nota za odległość będzie większa).
+
+//  Podobnie jest z belką startową: jeśli sędziowie zdecydują np. o jej obniżeniu, to zawodnik będzie miał      mniejszą prędkość, więc doleci bliżej, za co należą mu się dodatkowe punkty rekompensaty.
+
+//  Dla nas istotne jest, że obie te wartości to dwie liczby, które mogą być dodatnie lub ujemne.
+     
+    const max = Math.max(styleNotes[0], styleNotes[1], styleNotes[2], styleNotes[3], styleNotes[4]);
+    const min = Math.min(styleNotes[0], styleNotes[1], styleNotes[2], styleNotes[3], styleNotes[4]);
+      return styleNotes[0] + styleNotes[1] + styleNotes[2] + styleNotes[3] + styleNotes[4] - max - min;
+
+}
   
   module.exports = calculateStylePoints;
